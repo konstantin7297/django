3f054355ae1db261cd26c842858f80c94ae9929b
konstantin7297 <khromykh.kas@mail.ru>
2024-07-21 20:27:01 +0700

    shopapp: Views: categories, catalog created

--- myshop/shopapp/views.py
+++ myshop/shopapp/views.py
@@ -1,13 +1,55 @@
+from math import ceil
+
+from django.db.models import Q
+from django.http import HttpRequest
 from rest_framework.generics import ListAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
 
 
 class CategoriesView(ListAPIView):
-    pass
+    queryset = Category.objects.prefetch_related("subcategories").all()
+    serializer_class = CategorySerializer
 
 
 class CatalogView(ListAPIView):
-    pass
+    queryset = Product.objects.prefetch_related("tags", "orders", "images").select_related("category")
+
+    def get(self, request: Request, *args, **kwargs):
+        """  Данные фильтрации для шаблона
+        {
+            # 'filter[name]': '',
+            # 'filter[minPrice]': '0',
+            # 'filter[maxPrice]': '50000',
+            # 'filter[freeDelivery]': 'false',
+            # 'filter[available]': 'true',
+            # 'currentPage': '1',
+            # 'sort': 'price',
+            # 'sortType': 'inc',
+            # 'limit': '20'
+        }
+        """
+        data = request.query_params.dict()
+        start_row = max((int(data.get("currentPage", 1)) - 1) * int(data.get("limit", 20)), 1)
+        end_row = int(data.get("currentPage", 1)) * int(data.get("limit", 20)) + 1
+
+        result = self.get_queryset().filter(
+            Q(name__icontains=data.get('filter[name]')) if data.get('filter[name]') else Q(),
+            price__gte=float(data.get('filter[minPrice]')),
+            price__lte=float(data.get('filter[maxPrice]')),
+            freeDelivery=bool(data.get('filter[freeDelivery]').capitalize()),
+            count__gt=0 if bool(data.get('filter[available]').capitalize()) else Q(),
+        ).order_by('price' if data.get('sortType') == 'inc' else '-price')[start_row:end_row]
+
+        return Response({
+            "items": ProductSerializer(result, many=True).data,
+            "currentPage": data.get("currentPage", 1),
+            "lastPage": ceil(len(result) / 20)
+        })
 
 
 class ProductsPopularView(ListAPIView):
@@ -54,7 +96,7 @@ class BannersView(ListAPIView):
 
 class BasketView(APIView):
     def get(self, request, *args, **kwargs):
-        pass
+        return Response("")
 
     def post(self, request, *args, **kwargs):
         pass
@@ -64,4 +106,5 @@ class BasketView(APIView):
 
 
 class TagsView(ListAPIView):
-    pass
+    def get(self, request, *args, **kwargs):
+        return Response("")
