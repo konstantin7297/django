1aac88930e5298d573d5c50c9b09f87e4ff46709
konstantin7297 <khromykh.kas@mail.ru>
2024-07-23 17:46:38 +0700

    myshop: fix models

--- myshop/myauth/models.py
+++ myshop/myauth/models.py
@@ -9,6 +9,11 @@ def path_to_avatar(instance: "Profile", filename: str) -> str:
     return f"profiles/profile_{instance.pk}/avatar/{filename}"
 
 
+def avatar_size_validator(value):
+    if value.size > 2 * 1024 * 1024:
+        raise ValidationError("file size is too big, max: 2Mb")
+
+
 def payment_month_validator(value):
     if str(value) not in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]:
         raise ValidationError("Invalid payment month")
@@ -16,10 +21,12 @@ def payment_month_validator(value):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    fullName = models.CharField(null=True, blank=True, max_length=50)
-    email = models.EmailField(null=True, blank=True, unique=True)
+    fullName = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
     phone = models.IntegerField(null=True, blank=True, unique=True)
-    avatar = models.ImageField(null=True, blank=True, upload_to=path_to_avatar)
+    avatar = models.ImageField(null=True, blank=True, upload_to=path_to_avatar, validators=[
+        avatar_size_validator
+    ])
 
 
 class Payment(models.Model):
@@ -28,5 +35,9 @@ class Payment(models.Model):
     ])
     name = models.CharField(max_length=50)
     month = models.PositiveSmallIntegerField(validators=[payment_month_validator])
-    year = models.PositiveSmallIntegerField(validators=[MinValueValidator(datetime.now().year), MaxValueValidator(datetime.now().year + 3)])
-    code = models.PositiveSmallIntegerField(validators=[MinValueValidator(100), MaxValueValidator(999)])
+    year = models.PositiveSmallIntegerField(validators=[
+        MinValueValidator(datetime.now().year), MaxValueValidator(datetime.now().year + 3)
+    ])
+    code = models.PositiveSmallIntegerField(validators=[
+        MinValueValidator(100), MaxValueValidator(999)
+    ])
