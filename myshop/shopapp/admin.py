359f72e50bb14883b5df6fe2cd5ca140bddfb3b3
konstantin7297 <khromykh.kas@mail.ru>
2024-07-25 19:05:14 +0700

    shopapp urls is done

--- myshop/shopapp/admin.py
+++ myshop/shopapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+# from .models import Payment
+#
+#
+# @admin.register(Payment)
+# class PaymentAdmin(admin.ModelAdmin):
+#     list_display = ("number", "name", "month", "year", "code")
+#     list_display_links = ("number", "name")
+#     ordering = ("number", "name")
+#     search_fields = ("number", "name")
+#     fieldsets = [
+#         ("Payment", {
+#             "description": "Payment information",
+#             "fields": ("number", "name", "month", "year", "code"),
+#         })
+#     ]
--- myshop/shopapp/urls.py
+++ myshop/shopapp/urls.py
@@ -3,16 +3,17 @@ from django.urls import path
 from .views import (
     CategoriesView,
     CatalogView,
-    # ProductsPopularView,
+    ProductsPopularView,
     ProductsLimitedView,
     ProductReviewView,
     ProductByIdView,
     OrdersView,
     OrdersByIdView,
-    # SalesView,
-    # BannersView,
-    # BasketView,
+    SalesView,
+    BannersView,
+    BasketView,
     TagsView,
+    PaymentView,
 )
 
 app_name = 'shopapp'
@@ -20,14 +21,15 @@ app_name = 'shopapp'
 urlpatterns = [
     path('categories/', CategoriesView.as_view(), name="categories"),
     path('catalog/', CatalogView.as_view(), name="catalog"),
-    # path('products/popular/', ProductsPopularView.as_view(), name="products-popular"),
+    path('products/popular/', ProductsPopularView.as_view(), name="products-popular"),
     path('products/limited/', ProductsLimitedView.as_view(), name="products-limited"),
-    path('product/<int:id>/review/', ProductReviewView.as_view(), name="product-review"),
-    path('product/<int:id>/', ProductByIdView.as_view(), name="product-by-id"),
+    path('sales/', SalesView.as_view(), name="sales"),
+    path('banners/', BannersView.as_view(), name="banners"),
+    path('basket/', BasketView.as_view(), name="basket"),
     path('orders/', OrdersView.as_view(), name="orders"),
     path('orders/<int:id>/', OrdersByIdView.as_view(), name="orders-by-id"),
-    # path('sales/', SalesView.as_view(), name="sales"),
-    # path('banners/', BannersView.as_view(), name="banners"),
-    # path('basket/', BasketView.as_view(), name="basket"),
+    path("payment/", PaymentView.as_view(), name="payment"),
     path('tags/', TagsView.as_view(), name="tags"),
+    path('product/<int:id>/', ProductByIdView.as_view(), name="product-by-id"),
+    path('product/<int:id>/review/', ProductReviewView.as_view(), name="product-review"),
 ]
